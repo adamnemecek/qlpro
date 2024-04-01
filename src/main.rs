c78b91bac13576725460a3db4fa2cd25c64a3dd0
Adam Nemecek <adamnemecek@gmail.com>
2024-04-01 16:02:26 -0700

    stuffworks

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+mod keycodes;
+// use core_graphics::sys::CGEvent;
+pub use keycodes::*;
+
 use core_foundation::{
     runloop::CFRunLoop,
     // *,
 };
 use core_graphics::event::{
+    CGEvent,
     CGEventTap,
     CGEventTapLocation,
     CGEventTapOptions,
@@ -11,20 +16,27 @@ use core_graphics::event::{
     CGEventType,
 };
 
-fn listen() -> Result<CGEventTap<'static>, ()> {
+use core_graphics::event::CGKeyCode;
+
+fn keycode(e: &CGEvent) -> i64  {
+    e.get_integer_value_field(EventField::KEYBOARD_EVENT_KEYCODE)
+}
+
+fn listen(f : impl Fn(CGEvent)-> () +'static) -> Result<CGEventTap<'static>, ()> {
     let tap =
         CGEventTap::new(
             CGEventTapLocation::Session,
             CGEventTapPlacement::HeadInsertEventTap,
             CGEventTapOptions::Default,
             vec![CGEventType::KeyDown],
-            |_, _, event| {
-                println!(
-                    "event {:?}",
-                    event.get_integer_value_field(EventField::KEYBOARD_EVENT_KEYCODE)
-                );
+            move |_, _, ev| {
+                f(ev.to_owned());
+                // println!(
+                //     "event {:?}",
+                   
+                // );
 
-                Some(event.to_owned())
+                Some(ev.to_owned())
             },
         )?;
     let source = tap.mach_port.create_runloop_source(0)?;
@@ -38,7 +50,7 @@ fn listen() -> Result<CGEventTap<'static>, ()> {
 }
 
 fn main() {
-    let tap = listen().unwrap();
-
+    let tap = listen(|e| println!("{}", keycode(&e))).unwrap();
+    // MyEnum::A;
     CFRunLoop::run_current();
 }
