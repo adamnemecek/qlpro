384f73e5bd6796d68f4c5762f69cb912035aa8bb
Adam Nemecek <adamnemecek@gmail.com>
2024-04-01 19:17:09 -0700

    gets string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod keycodes;
+use core_foundation::base::{Boolean, CFIndex, CFRange};
 // use core_graphics::sys::CGEvent;
 pub use keycodes::*;
 
@@ -7,6 +8,11 @@ use core_foundation::{
     // *,
 };
 
+use core_foundation::string::{
+    kCFStringEncodingUTF8, CFString, CFStringGetBytes, CFStringGetCStringPtr, CFStringGetLength, CFStringRef
+};
+
+use std::borrow::Borrow;
 use std::process::{
     Command,
     Stdio,
@@ -62,12 +68,75 @@ fn listen(f: impl Fn(CGEvent) -> () + 'static) -> Result<CGEventTap<'static>, ()
 #[macro_use]
 extern crate objc;
 
+fn to_string(string_ref: CFStringRef) -> String {
+    // reference: https://github.com/servo/core-foundation-rs/blob/355740/core-foundation/src/string.rs#L49
+    unsafe {
+        let char_ptr = CFStringGetCStringPtr(string_ref, kCFStringEncodingUTF8);
+        if !char_ptr.is_null() {
+            let c_str = std::ffi::CStr::from_ptr(char_ptr);
+            return String::from(c_str.to_str().unwrap());
+        }
+
+        let char_len = CFStringGetLength(string_ref);
+
+        let mut bytes_required: CFIndex = 0;
+        CFStringGetBytes(
+            string_ref,
+            CFRange {
+                location: 0,
+                length: char_len,
+            },
+            kCFStringEncodingUTF8,
+            0,
+            false as Boolean,
+            std::ptr::null_mut(),
+            0,
+            &mut bytes_required,
+        );
+
+        // Then, allocate the buffer and actually copy.
+        let mut buffer = vec![b'\x00'; bytes_required as usize];
+
+        let mut bytes_used: CFIndex = 0;
+        CFStringGetBytes(
+            string_ref,
+            CFRange {
+                location: 0,
+                length: char_len,
+            },
+            kCFStringEncodingUTF8,
+            0,
+            false as Boolean,
+            buffer.as_mut_ptr(),
+            buffer.len() as CFIndex,
+            &mut bytes_used,
+        );
+
+        return String::from_utf8_unchecked(buffer);
+    }
+}
+
 unsafe fn front_most_application() {
     use cocoa::base::{id};
     // use objc::class;
     let workspace: id =  msg_send![class!(NSWorkspace), sharedWorkspace];
-    let frontApp: id =  msg_send![workspace, frontmostApplication] ;
-    println!("{:?}", frontApp);
+    let front_app: id =  msg_send![workspace, frontmostApplication] ;
+    let bundle_id: CFStringRef =  msg_send![front_app, bundleIdentifier] ;
+    // bundleID
+    let s = to_string(bundle_id);
+    println!("{:?}", s);
+    // let z = CFString::from_static_string("com.apple.quicklook.qlmanage");
+    // println!("{:?}", &z == *bundle_id);
+    // let s: std::borrow::Cow<'_, str> = bundleID.into();
+    // println!("{:?}", bundleID.as_str());
+    // let z = bundleID.as_ref().unwrap();
+    // let q= z.to_owned();
+    // let t =z.borrow();
+//    let qq: std::borrow::Cow<'static, str> = z.into();
+    // let r = std::borrow::Cow::from(bundleID);
+    // let o = bundleID.to_owned();
+    // let i = o.to_raw_parts();
+
 }
 
 fn main() {
